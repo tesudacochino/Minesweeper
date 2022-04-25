1159d6712d9edf6a1308d9c42d574e185f8e0729
smolero <smolero@gmail.com>
2022-04-25 17:09:00 +0200

    Siguiendo el tuto

---
+++ cell.py
@@ -0,0 +1,89 @@
+from tkinter import Button
+
+import random
+import settings
+
+class Cell:
+    all = []
+    def __init__(self, x, y, is_mine=False):
+        self.is_mine = is_mine
+        self.cell_btn_object = None
+        self.x = x
+        self.y = y
+
+        # Coleccion de todas las celdas
+        Cell.all.append(self)
+
+    def create_btn_object(self,location):
+        btn = Button(
+            location,
+            width=12,
+            height=4
+        )
+        btn.bind('<Button-1>', self.left_click_actions)
+        btn.bind('<Button-3>', self.rigth_click_actions)
+
+        self.cell_btn_object = btn
+
+    def left_click_actions(self, event):
+        if self.is_mine:
+            self.show_mine()
+        else:
+            if self.surrounded_cells_mines_length == 0:
+                for cell_obj in self.surrounded_cells:
+                    cell_obj.show_cell()
+            self.show_cell()
+
+    @property    
+    def surrounded_cells(self):
+        cells = [
+            self.get_cell_by_axis(self.x - 1, self.y - 1),
+            self.get_cell_by_axis(self.x - 1, self.y    ),
+            self.get_cell_by_axis(self.x - 1, self.y + 1),
+            self.get_cell_by_axis(self.x    , self.y - 1),
+            self.get_cell_by_axis(self.x + 1, self.y - 1),
+            self.get_cell_by_axis(self.x + 1, self.y    ),
+            self.get_cell_by_axis(self.x + 1, self.y + 1),
+            self.get_cell_by_axis(self.x    , self.y + 1),
+        ]
+        cells = [cell for cell in cells if cell is not None]
+        return cells
+
+    def show_cell(self):
+        print(self.surrounded_cells_mines_length)
+        self.cell_btn_object.configure(text=f"{self.surrounded_cells_mines_length}")
+
+    @property    
+    def surrounded_cells_mines_length(self):
+        counter = 0
+        for cell in self.surrounded_cells:
+            if cell.is_mine:
+                counter +=1
+        return counter
+
+    def get_cell_by_axis(self,x,y):
+        # Return  a object cell based on teh value x, y 
+        for cell in Cell.all:
+            if cell.x == x and cell.y == y:
+                return cell
+
+    def show_mine(self):
+        # end game
+        print("game LOST")
+        self.cell_btn_object.configure(bg='red')
+
+    def rigth_click_actions(self, event):
+        print("Rigth click")
+        print(event)
+
+    @staticmethod
+    def randomize_mines():
+        picked_cells = random.sample(
+            Cell.all,settings.MINES_COUNT
+        )
+
+        for picked_cell in picked_cells:
+            picked_cell.is_mine = True
+
+    def __repr__(self) -> str:
+        return f"Cell({self.x},{self.y})"
